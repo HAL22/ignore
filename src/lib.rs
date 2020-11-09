93da37129df40c0219ac36f8f5ad8dd565b5fc9c
Thethela Faltein <thethelafaltein@gmail.com>
2020-11-09 22:48:53 +0200

    Removed redundancies and refactored the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use rusqlite::{Connection, Result,Statement,NO_PARAMS};
 
-
 pub struct MyDbContext<'a>{
     pub connection: &'a Connection,
     pub tablename: String,
@@ -26,105 +25,61 @@ impl<'a> MyDbContext<'a>{
     }
 
     pub fn make_table(&mut self) -> Result<()>{
-
-        let create_table_string = format!("create table if not exists {} (
-            key text primary key,
-            value text not null unique
-        )",&self.tablename);
-
         if let None = &self.make_table_statement {
-
-            let stmt = self.connection.prepare(&create_table_string)?;
-
+            let stmt = self.connection.prepare("create table if not exists :table (
+                key text primary key,
+                value text not null unique
+            )")?;
             self.make_table_statement = Some(stmt);
         };
-
-        self.make_table_statement.as_mut().unwrap().execute(NO_PARAMS)?;
-
+        self.make_table_statement.as_mut().unwrap().execute_named(&[(":table",&self.tablename)])?;
         return Ok(());
-
     }
 
     pub fn create_gitignorefile(&mut self,key: &String,value: &String) -> Result<i64>{
-
-        let create_gitignorefile_string = format!("insert into {} (key, value) values (:key, :value)",&self.tablename);
-
         if let None = &self.create_gitignorefile_statement{
-
-            let stmt = self.connection.prepare(&create_gitignorefile_string)?;
-
+            let stmt = self.connection.prepare("insert into :table (key, value) values (:key, :value)")?;
             self.create_gitignorefile_statement = Some(stmt);
         };
-
         self.create_gitignorefile_statement.as_mut().unwrap().execute_named(
-            &[(":key",key),(":value",value)]
+            &[(":table",&self.tablename),(":key",key),(":value",value)]
         )?;
-
         return Ok(self.connection.last_insert_rowid());
-
     }
 
     pub fn read_gitignorefile(&mut self,key: &String) -> Result<Vec<String>>{
-
-        let read_gitignorefile_string = format!("select value from {} where key = :key",&self.tablename);
-
         if let None = &self.read_gitignorefile_statement{
-
-            let stmt = self.connection.prepare(&read_gitignorefile_string)?;
-
+            let stmt = self.connection.prepare("select value from :table where key = :key")?;
             self.read_gitignorefile_statement = Some(stmt);
         }
-
         let mut values = Vec::new();
-
-        let rows = self.read_gitignorefile_statement.as_mut().unwrap().query_map_named(&[(":key",key)],|row|{row.get(0)})?;
-
+        let rows = self.read_gitignorefile_statement.as_mut().unwrap().query_map_named(&[(":table",&self.tablename),(":key",key)],|row|{row.get(0)})?;
         for row in rows{
             values.push(row?);
         }
-
         return Ok(values);
-
     }
 
     pub fn update_gitignorefile(& mut self,key: &String,new_value: &String) -> Result<()>{
-
-        let update_gitignorefile_string = format!("update {} set value = :value where key = :key",&self.tablename);
-
         if let None = &self.update_gitignorefile_statement{
-
-            let stmt = self.connection.prepare(&update_gitignorefile_string)?;
-
+            let stmt = self.connection.prepare("update :table set value = :value where key = :key")?;
             self.update_gitignorefile_statement = Some(stmt);
         }
-
         self.update_gitignorefile_statement.as_mut().unwrap().execute_named(
-            &[(":key",key),(":value",new_value)]
+            &[(":table",&self.tablename),(":key",key),(":value",new_value)]
         )?;
-
         return Ok(());
-
     }
 
     pub fn delete_gitignorefile(& mut self,key: &String) -> Result<()>{
-
-        let delete_gitignorefile_string = format!("delete from {} where key = :key",&self.tablename);
-
         if let None = &self.delete_gitignorefile_statement{
-
-            let stmt = self.connection.prepare(& delete_gitignorefile_string)?;
-
+            let stmt = self.connection.prepare("delete from :table where key = :key")?;
             self.delete_gitignorefile_statement = Some(stmt);
-
         }
-
         self.delete_gitignorefile_statement.as_mut().unwrap().execute_named(
-            &[(":key",key)]
+            &[(":table",&self.tablename),(":key",key)]
         )?;
-
         return Ok(());
-
     }
-
 
 }
