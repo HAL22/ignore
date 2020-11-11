8dff79b36c601fe71a2992ac4fc6c20083eba408
Thethela Faltein <thethelafaltein@gmail.com>
2020-11-11 23:15:50 +0200

    Init file context

--- src/file_context.rs
+++ src/file_context.rs
@@ -1,4 +1,29 @@
-pub struct MyFileContext{
-    pub path: String,
-    
+use std::fs::File;
+
+pub struct MyFileContext<'a>{
+    pub gitignore_file: & 'a File,
+}
+
+impl<'a> MyFileContext<'a>{
+    pub fn new(file: &'a File) -> Self {
+       return  MyFileContext{
+            gitignore_file:file
+        }
+    }
+
+    pub fn make_or_amend_gitignore(ignores: &Vec<String>){
+
+        let ignores_iter = ignores.iter();
+
+        for f in ignores_iter{
+
+            
+
+        }
+
+
+
+    }
+
+
 }
